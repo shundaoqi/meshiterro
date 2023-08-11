4d739ae0ee83a872eff4bd53c38cbbf8bc802d88
shundaoqi <130890330+shundaoqi@users.noreply.github.com>
2023-08-11 07:06:10 +0000

    [finish] 3-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show, :edit]
   root to: 'homes#top'
   get '/homes/about', to: 'homes#about', as: 'about'
   devise_for :users
-  
-  resources :post_images, only: [:new, :index, :show, :create]
+
+  resources :post_images, only: [:new, :index, :show, :create, :destroy]
 end
